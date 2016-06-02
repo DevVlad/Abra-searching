a936bc12ab7ff0e3569d4052103c3086153fa9a6
Martin Schayna <martin.schayna@abra.eu>
2016-06-02 14:43:17 +0200

    fixing

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -1,80 +1,70 @@
-export const setFilter = (input) => {
-	handleRequest(0, input);
-	return {
-		type: 'SET_FILTER',
-		input: input
+import ApiService from '../services/apiservice.js';
+
+export function setFilter(filter) {
+	return dispatch => {
+		dispatch(init(filter));
+		if (filter !== '') {
+			dispatch(doRequest(filter, 0));
+		}
 	}
 }
 
-export const setLoading = () => {
+export function init(filter) {
 	return {
-		type: 'SET_LOADING',
-		hint: []
+		type: 'INIT',
+		filter
 	}
 }
 
-export const setLoaded = () => {
-	return {
-		type: 'SET_LOADED'
+const fields = ['jmeno', 'prijmeni', 'email', 'mobil', 'tel'];
+
+export function doRequest(filter, paging = 0) {
+	return dispatch => {
+		ApiService.getRequest({ 'add-row-count': true, 'start': paging, 'limit': 20 },
+			fields.map(f => `${f} like similar '${filter}'`).join(' or ')
+		).then(data => {
+			dispatch(processRequest(data.winstrom, filter, paging));
+		});
 	}
 }
 
-export const addHint = (field) => {
-	let pom = [];
-	field.map(x => { 
-		pom.concat(x.jmeno).concat(x.prijmeni);
-	})
+export function processRequest(data, filter, paging) {
+	return (dispatch, getState) => {
+		if (data.kontakt.length > 0 && getState().filter === filter) {
+			console.log('Applying the filter...');
+			const expr = new RegExp('\\b' + filter.split(' ').map(exp => '(' + exp + ')').join('.*\\b'), 'i');
+			const list = data.kontakt.filter(x =>
+				expr.test(x.jmeno) || expr.test(x.prijmeni) || expr.test(x.email) || expr.test(x.mobil) || expr.test(x.tel)
+			);
+			dispatch(addHint(list));
+			const count = paging + data.kontakt.length;
+			const totalCount = parseInt(data['@rowCount']);
+			const hintCount = getState().hint.length;
+			if (totalCount > count && hintCount < 10) {
+				dispatch(doRequest(filter, paging + data.kontakt.length));
+			}
+		}
+	}
+}
+
+export function addHint(list) {
 	return {
 		type: 'ADD_HINT',
-		hint: pom
+		hint: list
 	}
 }
 
-export const setInit = () => {
-	return {
-		input: '',
-		type: 'SET_INITIAL_STATE'
-	}
-}
-
-import ApiService from '../services/apiservice.js';
-const handleRequest = (paging, x) => {
-	//store - setLoading
-	ApiService.getRequest({
-		'add-row-count' : true,
-		'start' : paging
-		}, `jmeno like similar '${x}' or prijmeni like similar '${x}' or email like similar '${x}' or mobil like similar '${x}' or tel like similar '${x}'`).then( data => {
-		let pom = data.winstrom; //this.handleSoftFilter(paging, x, data.winstrom); 		
-		handleFilter(paging, x, data.winstrom);
-	});	
-}
-
-const handleFilter = (paging, input, data) => {
-	if (data.kontakt.length !== 0) {
-		let hint = [];
-		console.log('Applying the filter...');
-		const expr = new RegExp('\\b' + input.split(' ').map(exp => '(' + exp + ')').join('.*\\b'),'i'); 
-		data.kontakt.map(x => {
-			if ( expr.test(x.jmeno) || expr.test(x.prijmeni) || expr.test(x.email) || expr.test(x.mobil) || expr.test(x.tel) ) {
-				hint.push(x);
-			}
-		});
-		// store - addHint
-		decide({paging: paging, input: input, hint: hint, totalCount: parseInt(data['@rowCount'])});
-	} else {			
-		decide({paging: paging, input: input, hint: hint, totalCount: parseInt(data['@rowCount'])});
-	}
-}
-
-const decide = (data) => {
-	console.log('Deciding...', data)
-	// if ((obj.totalCount < 10 && obj.input === input) || (obj.hint.length >= 10 && obj. input === input)) {
-	// 	return {...obj, status: "OK"};
-	// } else if ((obj.hint.length < 10 && obj.paging < obj.totalCount) && obj.input === input) {
-	// 	return {...obj, status: "again"};
-	// } else {
-	// 	return console.log('Error in decider!');
-	// }
-}
+// export const setLoading = () => {
+// 	return {
+// 		type: 'SET_LOADING',
+// 		hint: []
+// 	}
+// }
+//
+// export const setLoaded = () => {
+// 	return {
+// 		type: 'SET_LOADED'
+// 	}
+// }
 
 
--- app/main.jsx
+++ app/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 // import App from './components/App.jsx';
 import Pokus from './components/pokus.jsx';
-
+import store from './store.js';
 
 ReactDOM.render(
-  <Pokus />,
-  document.body.appendChild(document.createElement('div'))
+	<Provider store={store}>
+		<Pokus />
+	</Provider>,
+	document.body.appendChild(document.createElement('div'))
 );
