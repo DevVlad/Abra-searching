391f2835fa64e59bf266dcca6c0f26839e82fbef
Vladimir Krejci <krejci.vladimir@icloud.com>
2016-06-02 16:58:16 +0200

    added hint length on output

--- app/reducers/reducer.jsx
+++ app/reducers/reducer.jsx
@@ -24,14 +24,6 @@ export default function reducer(state = initialState, action) {
     // case 'SET_LOADED':
     //   return Object.assign({}, state, {type: action.type, completed: true});//{...state, ...{type: action.type, completed: true}};
     //
-    // case 'ADD_HINT':
-    // let pom = [...state.hint, ...action.hint];
-    //   return Object.assign({}, state, {hint: pom, type: action.type});//{...state, ...{hint: action.hint, type: action.type}};
-    //
-    // case 'SET_INITIAL_STATE':
-    //   let newState = setInit();
-    //   return Object.assign({}, state, newState);//{...state, ...newState};
-    
     default:
       return state;
   }
