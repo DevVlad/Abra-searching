9d5a4f805d6aa75a3cb096c90b52a587551b01e4
Vladimir Krejci <krejci.vladimir@icloud.com>
2016-06-07 09:07:01 +0200

    no2 of immutable combineReducers

--- app/reducers/reducer.jsx
+++ app/reducers/reducer.jsx
@@ -41,37 +41,10 @@ function reducerLoading(state = initialStateLoading, action) {
 }
 
 const reducer = combineImmutableReducers(
-  Immutable.fromJS({
+  {
     filter: reducerFilter,
     loading: reducerLoading
-  }) 
+  } 
 );
 
 export default reducer;
-
-// const initialState = Immutable.fromJS(
-//   { 
-//     filter: '',
-//     hint: []
-//   }
-// );
-
-// export default function reducer(state = initialState, action) {
-//   switch (action.type) {
-//     case 'INIT':
-//       let stateInit = state.updateIn(['filter'], x => action.filter);
-//       stateInit = stateInit.updateIn(['hint'], x => Immutable.fromJS([]));
-//       return stateInit;
-
-//     case 'ADD_HINT':
-//       let stateAddHint = state.updateIn(['hint'], list => list.concat(Immutable.fromJS(action.hint)));
-//       return stateAddHint;
-
-//     case 'SET_LOADING':
-//       let stateLoading = state.updateIn(['loading'], x => action.loading);
-//       return stateLoading;
-
-//     default:
-//       return state;
-//   }
-// }
