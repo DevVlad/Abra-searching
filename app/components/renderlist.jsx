a9433f3e590dc6b9826d3e39e8ec198535ec6bf1
DevVlad <krejciv.zcu@gmail.com>
2016-06-10 12:05:03 +0200

    animate no.1

--- app/components/renderlist.jsx
+++ app/components/renderlist.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import './App.css'
 
 class ResultList extends React.Component {
 
-	renderList() {
-		return this.props.data.map((item, key) => {
-				return (
-					<li key={key}> <h4>{item.jmeno} {item.prijmeni}</h4> email: {item.email} mobil: {item.mobil} telefon: {item.tel}</li>
-				);
-			});
-	}
-
 	render() {
+
+		let items = this.props.data.map((item, key) => {
+					return (
+							<div key={key}> <h4>{item.jmeno} {item.prijmeni}</h4> email: {item.email} mobil: {item.mobil} telefon: {item.tel}</div>
+					);
+		});
 		return (
-			<ul>{this.renderList()}</ul>
+			<ul>
+				<ReactCSSTransitionGroup transitionName="anime" transitionEnterTimeout={1000} transitionLeaveTimeout={1000}>
+					{items}
+				</ReactCSSTransitionGroup>
+			</ul>
 		);
-	}
+		}
+
+
+	// renderList() {
+	// 	return this.props.data.map((item, key) => {
+	// 			return (
+	// 				<li key={key}> <h4>{item.jmeno} {item.prijmeni}</h4> email: {item.email} mobil: {item.mobil} telefon: {item.tel}</li>
+	// 			);
+	// 		});
+	// }
+	//
+	// render() {
+	// 	return (
+	// 		<ul>{this.renderList()}</ul>
+	// 	);
+	// }
 }
 export default ResultList;
