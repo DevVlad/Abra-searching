223148153dfc5387c1b8071ef841d02baea387bd
DevVlad <krejciv.zcu@gmail.com>
2016-06-15 15:04:55 +0200

    whisperer update

--- app/components/renderwhisperer.jsx
+++ app/components/renderwhisperer.jsx
@@ -1,28 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import $ from 'jquery';
+import select from 'selection-range';
 import RenderList from './renderlist.jsx';
 import RenderLoading from './renderloading.jsx';
 import RenderWhisperer from './renderwhisperer.jsx';
-import { connect } from 'react-redux';
 import { setFilter } from '../actions/actions.jsx';
 import { stateSelectorList } from '../selectors/selectors.jsx';
-import _ from 'lodash';
 import './App.css'
 
 class Whisperer extends React.Component{
 	constructor(props){
 		super(props);
-		this.inputRef = null;
-    this.whisper = [];
+    this.inputRef ='';
 	}
-
-  componentDidUpdate() {
-    const filter = this.props.filter;
-    this.whisper = this.props.hint.toJS().map(item => {
-      return this.getWhisperLine([item.jmeno, item.prijmeni], filter);
-    })
-    console.log('****************', this.whisper)
-
-  }
 
   getWhisperLine(whisper, filter) {
     if (whisper[0] !== '' && [...whisper[0]][0].toLowerCase() === [...filter][0]) {
@@ -41,39 +32,26 @@ class Whisperer extends React.Component{
     }
   }
 
-  // getWhisperLine(whisper, filter) {
-  //   if (whisper[0] !== '' && [...whisper[0]][0].toLowerCase() === filter) {
-  //     let name = whisper[0];
-  //     let lastName = whisper[1];
-  //     name = [...name].slice(1, name.length).join('');
-  //     return [name, lastName];
-  //   } else if (whisper[1] !== '' && [...whisper[1]][0].toLowerCase() === filter) {
-  //     let lastName = whisper[1];
-  //     lastName = [...lastName].slice(1, lastName.length).join('');
-  //     if (whisper[0] === '') {
-  //       return [lastName];
-  //     }else {
-  //       return [lastName, whisper[0]];
-  //     }
-  //   }
-  // }
-
 	filterChange(e) {
 		this.props.dispatch(setFilter(e.target.value));
-    this.whisper = [];
-    // if (this.props.hint.length === 0) {
-    //   this.whisper = [];
-    //   this.inputRef.value = this.props.filter;
-    // }
 	}
 
 	render(){
+    let whisper = '';
+    let rest = '';
+    if (this.props.hint.toJS().length > 0){
+      let pom = (this.props.hint.toJS()[0])
+      rest = this.getWhisperLine([pom.jmeno, pom.prijmeni], this.props.filter).join(' ');
+      whisper = this.props.filter + rest;
+      this.inputRef.setSelectionRange(this.props.filter.length, this.inputRef.value.length);
+    }
+
 		return (
       <div id="whisper">
   			<form role="formWhisperer">
   				<div>
   					<label className="labelWhisperer">Whisper for...</label>
-  	          <input id="whisperInput" ref={(ref) => this.inputRef = ref} type="text" placeholder="Search" onChange={this.filterChange.bind(this)} />
+  	          <input id="whisperInput" ref={(ref) => this.inputRef = ref} type="text" value={whisper} placeholder="Search" onChange={this.filterChange.bind(this)} />
           </div>
   			</form>
       </div>
