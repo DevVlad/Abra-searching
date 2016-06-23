47a6cb6827f76f152b496d7922f0d4cb328ecd92
DevVlad <krejciv.zcu@gmail.com>
2016-06-23 11:39:40 +0200

    store in browser

--- app/store.js
+++ app/store.js
@@ -1,9 +1,14 @@
 import reducer from './reducers/reducer.jsx';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import Immutable from 'immutable';
 
-const store = createStore(reducer, applyMiddleware(thunk));
+// const store = createStore(reducer, applyMiddleware(thunk));
+const store = createStore(reducer, compose(
+	applyMiddleware(thunk),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+));
+
 
 store.subscribe(() => {
 	console.log('--------------------------------------------------------------');
