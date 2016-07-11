5267fea0e32e0f09088791044c958bafe32c4b1c
Martin Schayna <martin.schayna@abra.eu>
2016-07-11 17:17:38 +0200

    Progress new start/stop

--- appNew/redux/ducks/progress.jsx
+++ appNew/redux/ducks/progress.jsx
@@ -3,14 +3,15 @@ import { createSelector } from 'reselect';
 
 const TEST = 'TEST';
 const SET_PROGRESS = 'SET_PROGRESS';
-function setCurrentState(bool) {
-	return {
-		type: SET_PROGRESS,
-		bool
-	};
-};
+const SET_STARTING = 'SET_STARTING';
+const SET_STARTED = 'SET_STARTED';
+const SET_STOP_TIMER = 'SET_STOP_TIMER';
+const INC_COUNTER = 'INC_COUNTER';
+const DEC_COUNTER = 'DEC_COUNTER';
 
-const getProgressState = (state) => state.getIn(['progress', 'loading']);
+// const getProgressState = (state) => state.getIn(['progress', 'loading']);
+
+const getProgress = state => state.get('progress');
 
 const Progress = {
 
@@ -18,54 +19,156 @@ const Progress = {
 	* ACTIONS
 	*/
 
-	setProgress(bool) {
-		progressBin.push(bool);
-		return (dispatch, getState) => {
-			let toNotice = [];
-			const timeID = setTimeout( () => {
-				if (bool !== getProgress(getState())) {
-					dispatch(setCurrentState(bool));
-				};
-			}, 100);
-			timesBin.push(timeID);
+	isStarting: createSelector(getProgress, progress => progress.get('starting')),
+	getCounter: createSelector(getProgress, progress => progress.get('counter')),
+	getStopTimer: createSelector(getProgress, progress => progress.get('stopTimer')),
+	isStarted: createSelector(getProgress, progress => progress.get('started')),
 
-			if(progressBin[progressBin.length-2] === bool) {
-				clearTimeout(timesBin[progressBin.length-1]);
-				timesBin[progressBin.length-1] = 0;
-			} else if(progressBin[progressBin.length-3] === bool) {
-				clearTimeout(timesBin[progressBin.length-2]);
-				timesBin[progressBin.length-2] = 0;
+	start() {
+		return (dispatch, getState) => {
+			dispatch(Progress.incCounter());
+			if (Progress.getCounter(getState()) == 1) {
+				if (!Progress.isStarting(getState())) {
+					setTimeout(() => {
+						dispatch(Progress.setStarted(Progress.getCounter(getState()) > 0));
+					}, 100);
+					dispatch(Progress.setStarting());
+				}
 			}
 		};
 	},
 
-	/*
-	* REDUCER
-	*/
+	stop() {
+		return (dispatch, getState) => {
+			dispatch(Progress.decCounter());
+			const counter = Progress.getCounter(getState());
+			if (counter == 0) {
+				const stopTimer = setTimeout(() => {
+					dispatch(Progress.setStarted(false));
+				}, 100);
+				dispatch(Progress.setStopTimer(stopTimer));
+			} else if (counter >= 1) {
+				const stopTimer = Progress.getStopTimer(getState());
+				if (stopTimer) {
+					clearTimeout(stopTimer);
+					dispatch(Progress.setStopTimer(0));
+				}
+			}
+		};
+	},
 
-	reducer(state = Immutable.fromJS({loading: false}), action) {
+	setStarting() {
+		return {
+			type: SET_STARTING,
+			starting: true
+		}
+	},
+
+	setStarted(started) {
+		return {
+			type: SET_STARTED,
+			started
+		};
+	},
+
+	setStopTimer(stopTimer) {
+		return {
+			type: SET_STOP_TIMER,
+			stopTimer
+		};
+	},
+
+	incCounter() {
+		return {
+			type: INC_COUNTER
+		}
+	},
+
+	decCounter() {
+		return {
+			type: DEC_COUNTER
+		}
+	},
+
+	// setProgress(bool) {
+	// 	progressBin.push(bool);
+	// 	return (dispatch, getState) => {
+	// 		let toNotice = [];
+	// 		const timeID = setTimeout( () => {
+	// 			if (bool !== getProgress(getState())) {
+	// 				console.log('dispatch: setCurrentState', bool);
+	// 				dispatch(Progress.setCurrentState(bool));
+	// 			};
+	// 		}, 100);
+	// 		timesBin.push(timeID);
+	//
+	// 		if(progressBin[progressBin.length-2] === bool) {
+	// 			clearTimeout(timesBin[progressBin.length-1]);
+	// 			timesBin[progressBin.length-1] = 0;
+	// 		} else if(progressBin[progressBin.length-3] === bool) {
+	// 			clearTimeout(timesBin[progressBin.length-2]);
+	// 			timesBin[progressBin.length-2] = 0;
+	// 		}
+	// 	};
+	// },
+	//
+	// setCurrentState(bool)
+	// {
+	// 	return {
+	// 		type: SET_PROGRESS,
+	// 		bool
+	// 	};
+	// },
+
+
+/*
+* REDUCER
+*/
+
+	reducer(state = Immutable.fromJS({}), action) {
 	  switch (action.type) {
 
-	    case SET_PROGRESS:
-				return state.setIn(['loading'], action.bool);
+	    // case SET_PROGRESS:
+			// 	return state.setIn(['loading'], action.bool);
 
-	    default:
-	      return state;
+		  case SET_STARTING:
+			  return state.set('starting', action.starting);
+
+		  case SET_STARTED:
+			  let newState = state;
+			  if (action.started) {
+				  newState = state.set('starting', false);
+			  } else {
+				  newState = state.set('stopTimer', 0);
+			  }
+			  return newState.set('started', action.started);
+
+		  case SET_STOP_TIMER:
+			  return state.set('stopTimer', action.stopTimer);
+
+		  case INC_COUNTER:
+			  return state.updateIn(['counter'], counter => counter + 1);
+
+		  case DEC_COUNTER:
+			  return state.updateIn(['counter'], counter => counter - 1);
+
+		  default:
+	        return state;
 	  }
 	},
 
 	getOwnState(state) {
 		return {
-			progress: getProgress(state)
+			progress: Progress.isStarted(state)
 		};
 	}
 
 };
 
-let progressBin = [];
-let timesBin = [];
-let counter = 0;
+// let progressBin = [];
+// let timesBin = [];
+// let counter = 0;
 
-const getProgress = createSelector(getProgressState, x => x);
+//const getProgress = createSelector(getProgressState, x => x);
 
 export default Progress;
--- appNew/tests/testProgress.js
+++ appNew/tests/testProgress.js
@@ -9,40 +9,40 @@ const mockStore = configureMockStore(middlewares);
 
 describe('async Progress actions', () => {
   it('should test behaviour in selected time', () => {
-    const store = mockStore(Immutable.fromJS({progress: {loading: false}}));
+    const store = mockStore(Immutable.fromJS({}));
     let pms = new Promise( (res, rej) => {
-      store.dispatch(Progress.setProgress(true));
+      store.dispatch(Progress.start());
       setTimeout( () => {
-        store.dispatch(Progress.setProgress(false));
+        store.dispatch(Progress.stop());
       }, 20);
       setTimeout( () => {
         res();
       }, 1000);
     });
     return pms.then( () => {
-      console.log(store.getActions(), store.getState())
-      expect(store.getActions()).toEqual([{ type: 'SET_PROGRESS', bool: true } ])
+      console.log(store.getActions(), store.getState());
+      expect(store.getActions()).toExclude({ type: 'SET_STARTED' });
       // expect(store.getState().toEqual(Immutable.fromJS({loading: false})))
     });
   });
 
 });
 
-describe('Progress Reducer', () => {
-  it('should return the initial state', () => {
-      expect(
-        Progress.reducer(undefined, {})
-      ).toEqual(
-        Immutable.fromJS({loading: false})
-      )
-  });
-
-  it('should handle SET_PROGRESS case', () => {
-    expect(
-      Progress.reducer(Immutable.fromJS({loading: false}), {
-        type: 'SET_PROGRESS',
-        bool: true
-      })).toEqual(Immutable.fromJS({loading: true}))
-  });
-
-});
+// describe('Progress Reducer', () => {
+//   it('should return the initial state', () => {
+//       expect(
+//         Progress.reducer(undefined, {})
+//       ).toEqual(
+//         Immutable.fromJS({loading: false})
+//       )
+//   });
+//
+//   it('should handle SET_PROGRESS case', () => {
+//     expect(
+//       Progress.reducer(Immutable.fromJS({loading: false}), {
+//         type: 'SET_PROGRESS',
+//         bool: true
+//       })).toEqual(Immutable.fromJS({loading: true}))
+//   });
+//
+// });
