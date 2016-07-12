a7eae905a3d3bc88788f89ac7679171912135896
DevVlad <krejciv.zcu@gmail.com>
2016-07-12 10:29:03 +0200

    basics tests

--- appNew/tests/testProgress.js
+++ appNew/tests/testProgress.js
@@ -8,22 +8,56 @@ const middlewares = [ thunk ];
 const mockStore = configureMockStore(middlewares);
 
 describe('async Progress actions', () => {
-  it('should test behaviour in selected time', () => {
+  it('start, 30stop, 30start, 150 res => should be started', () => {
     const store = mockStore(Immutable.fromJS({progress: {}}));
 
     let pms = new Promise( res => {
       store.dispatch(Progress.start());
       setTimeout( () => {
         store.dispatch(Progress.stop());
-      }, 20);
+      }, 30);
+      setTimeout( () => {
+        store.dispatch(Progress.start());
+      }, 30);
       setTimeout( () => {
         res();
-      }, 500);
+      }, 150);
     });
     return pms.then( () => {
       console.log(store.getActions());
       expect(store.getActions()).toExclude({ type: 'SET_STARTED' });
     });
   });
+
+  it('start, 30res => should not be started', () => {
+    const store = mockStore(Immutable.fromJS({progress: {}}));
+
+    let pms = new Promise( res => {
+      store.dispatch(Progress.start());
+      setTimeout( () => {
+        res();
+      }, 30);
+    });
+    return pms.then( () => {
+      console.log(store.getActions());
+      expect(store.getActions()).toExclude({ type: 'INC_COUNTER' });
+    });
+  });
+
+  it('start, 150res => should be started', () => {
+    const store = mockStore(Immutable.fromJS({progress: {}}));
+
+    let pms = new Promise( res => {
+      store.dispatch(Progress.start());
+      setTimeout( () => {
+        res();
+      }, 150);
+    });
+    return pms.then( () => {
+      console.log(store.getActions());
+      expect(store.getActions()).toExclude({ type: 'SET_STARTED' });
+    });
+  });
+
 
 });
